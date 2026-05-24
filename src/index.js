2ac7b17caa806d417a7d172b386c9033702c12ac
KhersonGirl <135836740+KhersonGirl@users.noreply.github.com>
2026-05-24 21:04:31 +0200

    improved user experience

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-  
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,10 +12,15 @@ function generatePoem(event) {
 
   let instructionsElement = document.querySelector("#instructions");
   let prompt = `Generate a poem about ${instructionsElement.value}`;
-  let context = "You are a romantic poet who writes short nice poems.Write each string with <br />. Follow user instructions please. Sing a poem with 'SheCodes AI' with <strong></strong> tag. ";
+  let context =
+    "You are a romantic poet who writes short nice poems.Write each string with <br />. Follow user instructions please. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning ";
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳Generating a poem about ${instructionsElement.value}...</div>`;
 
   let apiKey = "e2564f39e1f4c4aa4bf09cftodd0583e";
-  
+
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   axios.get(apiUrl).then(displayPoem);
